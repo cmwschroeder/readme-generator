4042363137534b20e1a6c88c073a5d7a9b027f0f
cmwschroeder <cmwschroeder@gmail.com>
2022-04-19 13:15:23 -0700

    added questions to inquirer prompt, takes in response but does nothing yet

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
 // TODO: Include packages needed for this application
-var inquirer = require('inquirer');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your directory?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description for your project: ',
+  },
+  {
+    type: 'confirm',
+    name: 'tableOfContents',
+    message: 'Would you like a table of contents?',
+    default: false,
+  },
+  {
+    type: 'input',
+    name: 'installationInstructions',
+    message: 'Enter any installation instructions for your project: ',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage instructions for your project: ',
+  },
+  {
+    type: 'input',
+    name: 'contributions',
+    message: 'Enter any contribution guidelines for your project: ',
+  },
+  {
+    type: 'input',
+    name: 'testInstructions',
+    message: 'Enter any test instructions for your project: ',
+  },
+  {
+    type: 'input',
+    name: 'githubName',
+    message: 'Enter your github username: ',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter the email to contact you about this project: ',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What license did you use?',
+    choices: ['MIT', 'APACHE', 'GPL'],
+  }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,6 +66,7 @@ function init() {
     inquirer
     .prompt(questions).then((answers) => {
         //process and do something with the answers.
+        console.log(answers);
     })
     .catch((error) => {
         if (error.isTtyError) {
