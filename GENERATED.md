70424d61e4c570fdf6a51adaf5a7cf57f190c711
cmwschroeder <cmwschroeder@gmail.com>
2022-04-19 19:45:04 -0700

    added using the input to give a readme file, without any license so far

---
+++ GENERATED.md
@@ -0,0 +1,35 @@
+# README Generator
+
+## Description
+
+This project generates a readme through inquirer
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
+
+## Installation
+
+npm install
+
+## Usage
+
+use it how u want
+
+## Contributing
+
+contact me
+
+## Tests
+
+test it
+
+## Questions
+If you have any questions you can reach me at:  
+  
+Github: cmwschroeder  
+Email: cmwschroeder@gmail.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,7 +20,38 @@ function renderLicenseSection(license) {
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+## Description
+
+${data.description}
+
+${data.tableOfContents ? `## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
+` : ''}
+## Installation
+
+${data.installationInstructions}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributions}
+
+## Tests
+
+${data.testInstructions}
+
+## Questions
+If you have any questions you can reach me at:  
+Github: ${data.githubName}  
+Email: ${data.email}`;
 }
 
 module.exports = generateMarkdown;
